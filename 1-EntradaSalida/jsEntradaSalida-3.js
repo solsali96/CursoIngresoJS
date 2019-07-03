5a35bd0dfc270611ade389837d8a1eedcd1d0c9c
Sol <52494953+solsali96@users.noreply.github.com>
2019-07-03 12:30:55 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var nombre;
+
+
+    nombre = document.getElementById("elNombre").value;
+
+    alert(nombre);
+
+    
 }
 
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,14 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{ 
+
+    var nombre;
+
+
+	nombre = prompt ("ingrese su nombre");
 
 }
+
+  document.getElementById("elNombre").value = nombre;
 
