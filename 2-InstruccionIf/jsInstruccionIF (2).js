4578deed9fb38e8ee4bdc47b3bc833666c67f36e
Sol <52494953+solsali96@users.noreply.github.com>
2019-07-10 12:23:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt (document.getElementById("edad").value);
 
+if (edad >= 18)
+
+{ alert("Usted es mayor de edad");
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,33 @@
 function mostrar()
 {
-//tomo la edad  
+ var edad;
+
+ edad = parseInt(document.getElementById("edad").value);
+
+ if (edad >= 18 ) {
+
+ alert ("es adulto");
+}
+ else 
+
+ { 
+     if (edad >= 13 && edad <= 17) }
+
+     alert ("Es adolescente");
+
+    }
+
+    else
+
+    { 
+        alert ("Es un niño");
+
+    }
+
+
+
+
+
 
 
 
