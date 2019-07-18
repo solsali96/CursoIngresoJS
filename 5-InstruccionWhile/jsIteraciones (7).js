5076631b8a0a78607f60f098648280b101d23a58
Sol <52494953+solsali96@users.noreply.github.com>
2019-07-18 12:27:31 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,34 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	while (seguir == 's') {
+		numero = parseInt (prompt ("Ingrese un numero: "));
+
+		while (isNaN(numero)) {
+
+			alert ("eso no es un numero");
+
+			numero = parseInt (prompt("Ingrese un numero"));
+
+
+
+	
+		}
+
+		acumulador = acumulador + numero;
+
+		contador ++;
+
+		seguir = prompt ("Quiere ingresar otro numero?");
+
+
+	
+	}
+
+
+
+	
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
