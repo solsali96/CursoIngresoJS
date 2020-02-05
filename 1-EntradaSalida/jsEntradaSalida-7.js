6633d59542d21f4df1f929231a9248c8fd7cf505
Sol <52494953+solsali96@users.noreply.github.com>
2020-02-05 10:21:42 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,93 +3,35 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	var num1;
+{	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
 
-    var num2;
+	primerNumero = document.getElementById('numeroUno').value;
+	primerNumero= parseInt(primerNumero); 
+	segundoNumero = document.getElementById('numeroDos').value;
+	segundoNumero = parseInt(segundoNumero);
 
-    var resultado;
+	resultado = primerNumero + segundoNumero;
 
-    num1 = document.getElementById("numeroUno").value;
+	console.log(resultado);
 
-    num1 = parseInt(num1)
-
-    num2 = document.getElementById("numeroDos").value;
-
-    num2 = parseInt(num2);
-
-    resultado = num1 +num2;
-;
-
-
-    alert ("la suma es " + resultado); 
 	
 }
 
 function restar()
-{   var num1;
-
-    var num2;
-
-    var resultado;
-
-    num1 = document.getElementById("numeroUno").value;
-
-    num1 = parseInt(num1)
-
-    num2 = document.getElementById("numeroDos").value;
-
-    num2 = parseInt(num2);
-
-    resultado = num1 - num2;
-;
-
-
-    alert ("la resta es " + resultado); 
+{
 	
 }
 
 function multiplicar()
 { 
-	var num1;
-
-    var num2;
-
-    var resultado;
-
-    num1 = document.getElementById("numeroUno").value;
-
-    num1 = parseInt(num1)
-
-    num2 = document.getElementById("numeroDos").value;
-
-    num2 = parseInt(num2);
-
-    resultado = num1 * num2;
-;
-
-
-    alert ("la multiplicacion es " + resultado); 
+	
 }
 
 function dividir()
 {
-	var num1;
-
-    var num2;
-
-    var resultado;
-
-    num1 = document.getElementById("numeroUno").value;
-
-    num1 = parseInt(num1)
-
-    num2 = document.getElementById("numeroDos").value;
-
-    num2 = parseInt(num2);
-
-    resultado = num1 / num2;
-;
-
-
-    alert ("la division es " + resultado); 
+	
 }
+
