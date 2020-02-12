7c193cd6998472dd4e9acc3ffea687c8566e5a14
Sol <52494953+solsali96@users.noreply.github.com>
2020-02-11 23:34:24 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,108 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+	var lamparas;
+	var precioLampara = 35;
+	var precioDescuento;
+	var precioFinal;
+	var Marca;
+
+	lamparas = document.getElementById('Cantidad').value;
+	lamparas = parseInt(lamparas);
+    Marca =document.getElementById('Marca').value;
+
+	if (lamparas >5)
+	{
+		precioLampara = precioLampara * lamparas;
+
+		precioDescuento =  precioLampara * 50;
+
+		precioFinal = precioDescuento /100;
+
+		document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+	}
+	
+	if ( lamparas == 5 && Marca == "ArgentinaLuz")
+	{
+		precioLampara = precioLampara * lamparas;
+
+	    precioDescuento =  precioLampara * 40;
+
+	    precioFinal = precioDescuento /100;
+
+	    document.getElementById('precioDescuento').value = precioLampara - precioFinal;	
+	}
+	else
+	{
+		if( lamparas == 5 && Marca != "ArgentinaLuz")
+		{
+			precioLampara = precioLampara * lamparas;
+
+	   	    precioDescuento =  precioLampara * 30;
+
+	    	precioFinal = precioDescuento /100;
+
+	   	    document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+		}
+	}
+	if ( lamparas == 4 && Marca == "ArgentinaLuz" || Marca == "FelipeLamparas")
+	{
+		precioLampara = precioLampara * lamparas;
+
+	    precioDescuento =  precioLampara * 25;
+
+	    precioFinal = precioDescuento /100;
+
+	    document.getElementById('precioDescuento').value = precioLampara - precioFinal;	
+	}
+	else
+	{
+		if ( lamparas == 4 && Marca != "ArgentinaLuz" || Marca != "FelipeLamparas")
+		{
+			precioLampara = precioLampara * lamparas;
+
+	        precioDescuento =  precioLampara * 20;
+
+	        precioFinal = precioDescuento /100;
+
+	        document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+		}   
+	}
+	if (lamparas == 3 && Marca == "ArgentinaLuz")
+	{
+		precioLampara = precioLampara * lamparas;
+
+		precioDescuento =  precioLampara * 15;
+
+		precioFinal = precioDescuento /100;
+
+		document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+	}
+	else
+	{
+		if (lamparas == 3 && Marca == "FelipeLamparas")
+		{
+			precioLampara = precioLampara * lamparas;
+
+		    precioDescuento =  precioLampara * 10;
+
+		    precioFinal = precioDescuento /100;
+
+		    document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+		}
+		else
+		{
+			if (lamparas == 3 && Marca != "FelipeLamparas" || Marca != "ArgentinaLuz")
+			{
+				precioLampara = precioLampara * lamparas;
+
+		        precioDescuento =  precioLampara * 5;
+
+		        precioFinal = precioDescuento /100;
+
+		        document.getElementById('precioDescuento').value = precioLampara - precioFinal;
+			}
+		}
+	}
+	
 }
